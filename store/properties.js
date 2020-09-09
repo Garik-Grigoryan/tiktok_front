b6a1e2b7521dfd065b8f78e6e6fddafa45324592
Maga Taylor <magataylor01@gmail.com>
2020-09-09 14:16:56 +0400

    commit

--- store/properties.js
+++ store/properties.js
@@ -18,24 +18,24 @@ export const mutations = {
 
 export const actions = {
   async fetch({commit}) {
-    const properties = await this.$axios.$get('http://localhost:8000/api/productProperties/get');
+    const properties = await this.$axios.$get('http://tiktokback.neoteric-software.com/api/productProperties/get');
     commit('setProperties', properties);
   },
   async getProperty({commit}, [id]){
-    const property = await this.$axios.$get(`http://localhost:8000/api/productProperties/get/${id}`);
+    const property = await this.$axios.$get(`http://tiktokback.neoteric-software.com/api/productProperties/get/${id}`);
     commit('setProperty', property)
   },
   async productProperty({commit}, [id]){
-    const property = await this.$axios.$get(`http://localhost:8000/api/productProperties/productProperty/${id}`);
+    const property = await this.$axios.$get(`http://tiktokback.neoteric-software.com/api/productProperties/productProperty/${id}`);
     commit('setProductProperty', property);
     return property;
   },
   async addProperty({commit}, [type, value, name_en, name_am, name_ru]){
-    const property = await this.$axios.$post(`http://localhost:8000/api/productProperties/add`, {'type': type, 'value': value, 'name_en': name_en, 'name_am': name_am, 'name_ru': name_ru});
+    const property = await this.$axios.$post(`http://tiktokback.neoteric-software.com/api/productProperties/add`, {'type': type, 'value': value, 'name_en': name_en, 'name_am': name_am, 'name_ru': name_ru});
     commit('setProperty', property)
   },
   async updateProperty({commit}, [id, type, value, name_en, name_am, name_ru]){
-    const property = await this.$axios.$put(`http://localhost:8000/api/productProperties/update/${id}`, {'type': type, 'value': value, 'name_en': name_en, 'name_am': name_am, 'name_ru': name_ru});
+    const property = await this.$axios.$put(`http://tiktokback.neoteric-software.com/api/productProperties/update/${id}`, {'type': type, 'value': value, 'name_en': name_en, 'name_am': name_am, 'name_ru': name_ru});
     commit('setProperty', property)
   },
   async delete({commit}, [id]){
